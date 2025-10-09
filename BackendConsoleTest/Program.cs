45235b800021e3c5836c0324c094ff56015c7515
coreyellow <lancemiguel@gmail.com>
2025-10-09 17:10:12 -0400

    Redo backend files

---
+++ BackendConsoleTest/Program.cs
@@ -0,0 +1,10 @@
+﻿using BackendLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ShopTicket pdf = new ShopTicket();
+        Console.WriteLine(pdf.NumberOfPages);
+    }
+}
