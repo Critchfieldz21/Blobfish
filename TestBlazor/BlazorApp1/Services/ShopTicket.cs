7d45e97355e169d185b0e0169def715194e71c1d
coreyellow <lancemiguel@gmail.com>
2025-10-29 22:40:52 -0400

    Update Services to latest in backend branch

--- TestBlazor/BlazorApp1/Services/ShopTicket.cs
+++ TestBlazor/BlazorApp1/Services/ShopTicket.cs
@@ -17,11 +17,11 @@ namespace BackendLibrary
         public int PiecesRequired { get; set; }                    // Pieces required from the title block labelled "PIECES REQ'D:".
         public decimal Weight { get; set; }                        // Weight from the title block labelled "WEIGHT:".
         public string DesignNumber { get; set; }                   // Design number from the title block labelled "DESIGN:".
-        //public int RectanglePage { get; set; }                              // 0-based index of the page containing form and section view rectangles.
-        //public double FormViewRectangleX { get; set; }             // Distance from left edge of PDF to left edge of the form view rectangle (inches).
-        //public double FormViewRectangleY { get; set; }             // Distance from top edge of PDF to top edge of the form view rectangle (inches).
-        //public double FormViewRectangleWidth { get; set; }         // Width of the form view rectangle (inches).
-        //public double FormViewRectangleHeight { get; set; }        // Height of the form view rectangle (inches).
+        public int RectanglePage { get; set; }                              // 0-based index of the page containing form and section view rectangles.
+        public double FormViewRectangleX { get; set; }             // Distance from left edge of PDF to left edge of the form view rectangle (inches).
+        public double FormViewRectangleY { get; set; }             // Distance from top edge of PDF to top edge of the form view rectangle (inches).
+        public double FormViewRectangleWidth { get; set; }         // Width of the form view rectangle (inches).
+        public double FormViewRectangleHeight { get; set; }        // Height of the form view rectangle (inches).
         //public double SectionViewRectangleX { get; set; }          // Distance from left edge of PDF to left edge of the section view rectangle (inches).
         //public double SectionViewRectangleY { get; set; }          // Distance from top edge of PDF to top edge of the section view rectangle (inches).
         //public double SectionViewRectangleWidth { get; set; }      // Width of the section view rectangle (inches).
@@ -31,7 +31,7 @@ namespace BackendLibrary
         {
             try
             {
-                byte[] PdfBytes = File.ReadAllBytes(pdfPath);
+                PdfBytes = File.ReadAllBytes(pdfPath);
 
                 // Use PdfSharp PdfReader to initialize a PdfDocument object off of the input file path
                 PdfDocument pdf = PdfReader.Open(pdfPath);
@@ -39,7 +39,8 @@ namespace BackendLibrary
                 // Use PdfPig to extract text from pdf
                 PdfTextExtractor pdfTextExtractor = new PdfTextExtractor(pdfPath);
 
-                InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithPdfPath(pdfPath));
+                //Pass pdfPath for page name extraction
+                InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithPdfPath(pdfPath), pdfPath);
             }
             catch (Exception ex)
             {
@@ -59,7 +60,34 @@ namespace BackendLibrary
 
                 // Use PdfPig to extract text from pdf
                 PdfTextExtractor pdfTextExtractor = new PdfTextExtractor(pdfBytes);
-                InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithFileName(fileName));
+
+                //null for pdfPath since we don't have a real file path
+                InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithFileName(fileName), null);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error initializing shop ticket: {ex.Message}", ex);
+            }
+        }
+
+        public ShopTicket(String fileName, byte[] pdfBytes, string modelPath)
+        {
+            try
+            {
+                PdfBytes = pdfBytes;
+
+                // Use PdfSharp PdfReader to initialize a PdfDocument object off of pdf byte array
+                MemoryStream stream = new MemoryStream(pdfBytes);
+                PdfDocument pdf = PdfReader.Open(stream, PdfDocumentOpenMode.Import);
+
+                // Use PdfPig to extract text from pdf
+                PdfTextExtractor pdfTextExtractor = new PdfTextExtractor(pdfBytes);
+
+                //null for pdfPath since we don't have a real file path
+                InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithFileName(fileName), null);
+
+                (RectanglePage, FormViewRectangleX, FormViewRectangleY, FormViewRectangleWidth, FormViewRectangleHeight) = Detect.GetRectInfo(modelPath, fileName, stream);
+
             }
             catch (Exception ex)
             {
@@ -68,7 +96,7 @@ namespace BackendLibrary
         }
 
         // Combine shared logic between constructors
-        private void InitializeFromPdf(PdfDocument pdf, PdfTextExtractor pdfTextExtractor, PdfFileNameExtractor pdfFileNameExtractor)
+        private void InitializeFromPdf(PdfDocument pdf, PdfTextExtractor pdfTextExtractor, PdfFileNameExtractor pdfFileNameExtractor, string? pdfPath)
         {
             // OwnerPassword property needs a password to set SecuritySettings
             pdf.SecuritySettings.OwnerPassword = "admin";
@@ -94,6 +122,14 @@ namespace BackendLibrary
                 PiecesRequired = pdfTextExtractor.ExtractPiecesRequired();
                 Weight = pdfTextExtractor.ExtractWeight();
                 DesignNumber = pdfTextExtractor.ExtractDesignNumber();
+
+                ////Extract Page Names using PdfPageNameExtractor
+                //if (!string.IsNullOrEmpty(pdfPath) && File.Exists(pdfPath))
+                //{
+                //    var pageNameExtractor = new PdfPageNameExtractor(72); // top 1 inch band
+                //    TimeSpan elapsed;
+                //    PageNames = pageNameExtractor.ExtractAll(pdfPath, out elapsed).ToList();
+                //}
             }
             catch (Exception ex)
             {
@@ -113,18 +149,23 @@ namespace BackendLibrary
                 "ControlNumbers: " + (ControlNumbers != null ? string.Join(", ", ControlNumbers) : "null") + "\n" +
                 "PiecesRequired: " + PiecesRequired + "\n" +
                 "Weight: " + Weight + " lb\n" +
-                "DesignNumber: " + DesignNumber + "\n";
-
+                "RectanglePage: " + RectanglePage + "\n" +
+                "FormViewRectangleX: " + FormViewRectangleX + "\n" +
+                "FormViewRectangleY: " + FormViewRectangleY + "\n" +
+                "FormViewRectangleWidth: " + FormViewRectangleWidth + "\n" +
+                "FormViewRectangleHeight: " + FormViewRectangleHeight + "\n";
+            //"PageNames: " + (PageNames.Count > 0 ? string.Join(" | ", PageNames) : "null") + "\n";
 
             return str;
         }
 
         public void Info()
         {
+            string str = NumberOfPages.ToString() + " | " + FileName + " | " + FileNamePieceMark + " | " + ProjectNumber +
+                         " | " + ProjectName + " | " + FileContentPieceMark + " | " + PiecesRequired.ToString() +
+                         " | " + Weight.ToString() + " lb | " + DesignNumber;
 
-            string str = NumberOfPages.ToString() + " | " + FileName + " | " + FileNamePieceMark + " | " + ProjectNumber + " | " + ProjectName + " | " + FileContentPieceMark + " | " + PiecesRequired.ToString() + " | " + Weight.ToString() + " lb | " + DesignNumber;
-
-            String filepath = "/Users/zacharycritchfield/Desktop/DB/ShopTicketInfo.txt";
+            String filepath = "/Users/zacharycritchfield/Documents/GitHub/Blobfish/SQL3cs/ShopTicketInfo.txt";
 
             List<String> lines = new List<String>();
             if (File.Exists(filepath))
@@ -138,8 +179,6 @@ namespace BackendLibrary
                 lines.Add(str);
                 File.WriteAllLines(filepath, lines);
             }
-
-
         }
     }
 }
