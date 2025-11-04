7af71c81559748b4bd5f50bbfac8aad978758859
coreyellow <lancemiguel@gmail.com>
2025-11-03 19:17:59 -0500

    Adjust comments on properties

--- BackendConsoleTest/Program.cs
+++ BackendConsoleTest/Program.cs
@@ -12,10 +12,9 @@ class Program
         //String filePath = "/Users/zacharycritchfield/Desktop/ShopTickets/ShopTicketData/pdfs/20-NE0881-W001_P2.pdf";
         //String filePath = "/Users/zacharycritchfield/Desktop/ShopTickets/ShopTicketData/pdfs/24-NE1096-DT023_P0.pdf";
         //string filePath = "/Users/zacharycritchfield/Desktop/ShopTickets/ShopTicketData/pdfs/25-NE1203.01-W015_P2.pdf";
-        //String filePath = "C:/Users/lance/Documents/ShopTicketData/pdfs/20-NE0881-W050_P2.pdf";
-        String filePath = "";
+        String filePath = "C:/Users/lance/Documents/ShopTicketData/pdfs/24-NE1173-W038_P2.pdf";
+        //String filePath = "";
 
-        Console.WriteLine(Directory.GetCurrentDirectory());
         byte[] pdfBytes = File.ReadAllBytes(filePath);
         String pdfName = Path.GetFileNameWithoutExtension(filePath);
         ShopTicket pdf = new ShopTicket(pdfName, pdfBytes, "../../../../BackendLibrary/best.onnx");
