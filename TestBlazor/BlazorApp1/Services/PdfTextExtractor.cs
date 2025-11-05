33ef6a37ca112db3953e6ed7638e4b102e60fc48
coreyellow <lancemiguel@gmail.com>
2025-11-05 10:18:46 -0500

    Update services

--- TestBlazor/BlazorApp1/Services/PdfTextExtractor.cs
+++ TestBlazor/BlazorApp1/Services/PdfTextExtractor.cs
@@ -1,5 +1,7 @@
 ﻿using UglyToad.PdfPig;
+using UglyToad.PdfPig.Annotations;
 using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.Geometry;
 
 namespace BackendLibrary
 {
@@ -23,13 +25,129 @@ namespace BackendLibrary
             //foreach (Page page in pages)
             //{
             //    IEnumerable<Word> words = page.GetWords();
+            //    IEnumerable<Annotation> annotations = page.GetAnnotations();
+
             //    foreach (Word word in words)
             //    {
             //        Console.WriteLine($"Word: {word.Text}, Bounding Box: {word.BoundingBox}");
             //    }
+            //    foreach (Annotation annotation in annotations)
+            //    {
+            //        Console.WriteLine($"Annotation: {annotation.Content}");
+            //    }
             //}
         }
 
+        public string[] ExtractPageNames()
+        {
+            List<String> resultList = new List<String>();
+            List<String> annotationStrings = new List<String>();
+            foreach (Page page in pages)
+            {
+                IEnumerable<Annotation> annotations = page.GetAnnotations();
+
+                // PDFs can either have AutoCAD annotations to signify view labels or no annotations
+                // Move on to word search if there are no annotations
+                if (annotations.Count() == 0)
+                {
+                    resultList.Add(ExtractPageNameNoAnnotations(page));
+                    continue;
+                }
+
+                annotationStrings.Clear();
+                foreach (Annotation annotation in annotations)
+                {
+                    // Annotation must be underlined to be considered
+                    if (annotation.Content is null || !(annotation.Content.Contains("%%U", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        continue;
+                    }
+
+                    String content = annotation.Content.Replace("%%U", "").Trim().ToUpper();
+                    annotationStrings.Add(content);
+                }
+
+                if (annotationStrings.Contains("FORM VIEW"))
+                {
+                    resultList.Add("FormView");
+                }
+                else if (annotationStrings.Contains("FOAM DRAWING"))
+                {
+                    resultList.Add("FoamDrawing");
+                }
+                else if (annotationStrings.Contains("REVEAL DRAWING"))
+                {
+                    resultList.Add("RevealDrawing");
+                }
+                else // Move on to word search if annotations are invalid
+                {
+                    resultList.Add(ExtractPageNameNoAnnotations(page));
+                }
+            }
+            return resultList.ToArray();
+        }
+
+        private string ExtractPageNameNoAnnotations(Page page)
+        {
+            IEnumerable<Word> words = page.GetWords();
+            List<Word> formWords = (from Word word in words
+                                    where word.Text.Equals("FORM", StringComparison.OrdinalIgnoreCase)
+                                    select word).ToList();
+            List<Word> drawingWords = (from Word word in words
+                                       where word.Text.Equals("DRAWING", StringComparison.OrdinalIgnoreCase)
+                                       select word).ToList();
+            foreach (Word word in formWords)
+            {
+                Word? foundWord = FindWordNextTo(word, words, 20, 50, -1, 1);
+                if (foundWord is null)
+                {
+                    if (word.BoundingBox.Left > 900)
+                    {
+                        continue;
+                    }
+                    List<Word> foundWords = FindWordsNextTo(word, words, -80, -20, -1, 1);
+                    foreach (Word foundWord2 in foundWords)
+                    {
+                        if (string.Equals(foundWord2.Text, "TOP", StringComparison.OrdinalIgnoreCase))
+                        {
+                            return "FormView";
+                        }
+                    }
+                    continue;
+                }
+                if (string.Equals(foundWord.Text, "VIEW", StringComparison.OrdinalIgnoreCase))
+                {
+                    return "FormView";
+                }
+            }
+            foreach (Word word in drawingWords)
+            {
+                Word? foundWord = FindWordNextTo(word, words, -80, -20, -1, 1);
+                if (foundWord is null)
+                {
+                    continue;
+                }
+                if (string.Equals(foundWord.Text, "FOAM", StringComparison.OrdinalIgnoreCase))
+                {
+                    return "FoamDrawing";
+                }
+                else if (string.Equals(foundWord.Text, "REVEAL", StringComparison.OrdinalIgnoreCase))
+                {
+                    return "RevealDrawing";
+                }
+            }
+
+            foreach (Word word in words)
+            {
+                if (word.Text.Contains("REVEAL", StringComparison.OrdinalIgnoreCase))
+                {
+                    return "RevealDrawing";
+                }
+            }
+
+            return "UnknownPage";
+            //throw new NullReferenceException($"Failed to get PageNames - Page {page.Number} has no valid view label");
+        }
         public string ExtractProjectNumber()
         {
             foreach (Page page in pages)
@@ -142,6 +260,8 @@ namespace BackendLibrary
                     {
                         continue;
                     }
+
+                    // Search method for when the word after "CONTROL" or "CTRL" is "NUMBER" or "NUMBER:"
                     searchTerms = new List<String> { "NUMBER", "NUMBER:" };
                     if (searchTerms.Any(searchTerm => searchTerm.Equals(foundWord.Text, StringComparison.OrdinalIgnoreCase)))
                     {
@@ -151,6 +271,8 @@ namespace BackendLibrary
                             return null;
                         }
                     }
+
+                    // Search method for when the word after "CONTROL" or "CTRL" is "NO.", "NO:", or "NO.:"
                     searchTerms = new List<String> { "NO.", "NO:", "NO.:" };
                     if (searchTerms.Any(searchTerm => searchTerm.Equals(foundWord.Text, StringComparison.OrdinalIgnoreCase)))
                     {
@@ -167,12 +289,31 @@ namespace BackendLibrary
                             return null;
                         }
                     }
+
                     if (controlnumWords is not null)
                     {
-                        controlnumstrList.AddRange(controlnumWords.Select(word => word.Text));
+                        // Add each controlnumWord to controlnumstrList if it is not overlapping with any other word
+                        for (int i = 0; i < controlnumWords.Count; i++)
+                        {
+                            bool isOverlapping = false;
+                            for (int j = i + 1; j < controlnumWords.Count; j++)
+                            {
+                                // Check overlapping bounding boxes to avoid duplicates
+                                if (controlnumWords[i].BoundingBox.IntersectsWith(controlnumWords[j].BoundingBox))
+                                {
+                                    isOverlapping = true;
+                                    break;
+                                }
+                            }
+                            if (!isOverlapping)
+                            {
+                                controlnumstrList.Add(controlnumWords[i].Text);
+                            }
+                        }
+
                         controlnumstrList.ForEach(str => resultList.AddRange(str.Split(',', StringSplitOptions.RemoveEmptyEntries)));
 
-                        return resultList.Distinct().ToArray();
+                        return resultList.ToArray();
                     }
                 }
             }
--- TestBlazor/BlazorApp1/Services/ShopTicket.cs
+++ TestBlazor/BlazorApp1/Services/ShopTicket.cs
@@ -5,23 +5,24 @@ namespace BackendLibrary
 {
     public class ShopTicket
     {
-        public byte[] PdfBytes;
-        public int NumberOfPages { get; set; }                              // Number of pages in the PDF file.
-        //public string[] PageNames { get; set; }                    // Page names extracted from view labels.
-        public string FileName { get; set; }                       // File name of the PDF file.
-        public string? FileNamePieceMark { get; set; }                      // Piece Mark extracted from the file name.
-        public string ProjectNumber { get; set; }                  // Project Number from the title block labelled "JOB NO.".
-        public string ProjectName { get; set; }                    // Project Name from the title block labelled "PROJECT:".
-        public string FileContentPieceMark { get; set; }           // Piece Mark from the title block labelled "PIECE MARK".
-        public string[]? ControlNumbers { get; set; }                       // Control numbers from the square above the title block labelled "CONTROL NO.:".
-        public int PiecesRequired { get; set; }                    // Pieces required from the title block labelled "PIECES REQ'D:".
-        public decimal Weight { get; set; }                        // Weight from the title block labelled "WEIGHT:".
-        public string DesignNumber { get; set; }                   // Design number from the title block labelled "DESIGN:".
-        public int RectanglePage { get; set; }                              // 0-based index of the page containing form and section view rectangles.
-        public double FormViewRectangleX { get; set; }             // Distance from left edge of PDF to left edge of the form view rectangle (inches).
-        public double FormViewRectangleY { get; set; }             // Distance from top edge of PDF to top edge of the form view rectangle (inches).
-        public double FormViewRectangleWidth { get; set; }         // Width of the form view rectangle (inches).
-        public double FormViewRectangleHeight { get; set; }        // Height of the form view rectangle (inches).
+        public byte[] PdfBytes { get; }                             // Bytearray of PDF file
+        public DateTime dateTimeExtracted { get; }                  // Date and time when ShopTicket was constructed
+        public int NumberOfPages { get; set; }                      // Number of pages in the PDF file.
+        public string[] PageNames { get; set; }                     // Page names extracted from view labels.
+        public string FileName { get; set; }                        // File name of the PDF file.
+        public string? FileNamePieceMark { get; set; }              // Piece Mark extracted from the file name.
+        public string ProjectNumber { get; set; }                   // Project Number from the title block labelled "JOB NO.".
+        public string ProjectName { get; set; }                     // Project Name from the title block labelled "PROJECT:".
+        public string FileContentPieceMark { get; set; }            // Piece Mark from the title block labelled "PIECE MARK".
+        public string[]? ControlNumbers { get; set; }               // Control numbers from the square above the title block labelled "CONTROL NO.:".
+        public int PiecesRequired { get; set; }                     // Pieces required from the title block labelled "PIECES REQ'D:".
+        public decimal Weight { get; set; }                         // Weight from the title block labelled "WEIGHT:".
+        public string DesignNumber { get; set; }                    // Design number from the title block labelled "DESIGN:".
+        public int RectanglePage { get; set; }                      // 0-based index of the page containing form and section view rectangles.
+        public double FormViewRectangleX { get; set; }              // Distance from left edge of PDF to left edge of the form view rectangle (inches).
+        public double FormViewRectangleY { get; set; }              // Distance from top edge of PDF to top edge of the form view rectangle (inches).
+        public double FormViewRectangleWidth { get; set; }          // Width of the form view rectangle (inches).
+        public double FormViewRectangleHeight { get; set; }         // Height of the form view rectangle (inches).
         //public double SectionViewRectangleX { get; set; }          // Distance from left edge of PDF to left edge of the section view rectangle (inches).
         //public double SectionViewRectangleY { get; set; }          // Distance from top edge of PDF to top edge of the section view rectangle (inches).
         //public double SectionViewRectangleWidth { get; set; }      // Width of the section view rectangle (inches).
@@ -39,8 +40,7 @@ namespace BackendLibrary
                 // Use PdfPig to extract text from pdf
                 PdfTextExtractor pdfTextExtractor = new PdfTextExtractor(pdfPath);
 
-                //Pass pdfPath for page name extraction
-                InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithPdfPath(pdfPath), pdfPath);
+                InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithPdfPath(pdfPath));
             }
             catch (Exception ex)
             {
@@ -61,8 +61,7 @@ namespace BackendLibrary
                 // Use PdfPig to extract text from pdf
                 PdfTextExtractor pdfTextExtractor = new PdfTextExtractor(pdfBytes);
 
-                //null for pdfPath since we don't have a real file path
-                InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithFileName(fileName), null);
+                InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithFileName(fileName));
             }
             catch (Exception ex)
             {
@@ -83,11 +82,10 @@ namespace BackendLibrary
                 // Use PdfPig to extract text from pdf
                 PdfTextExtractor pdfTextExtractor = new PdfTextExtractor(pdfBytes);
 
-                //null for pdfPath since we don't have a real file path
-                InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithFileName(fileName), null);
+                InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithFileName(fileName));
 
                 (RectanglePage, FormViewRectangleX, FormViewRectangleY, FormViewRectangleWidth, FormViewRectangleHeight) = Detect.GetRectInfo(modelPath, fileName, stream);
-
+                dateTimeExtracted = DateTime.Now;
             }
             catch (Exception ex)
             {
@@ -96,7 +94,7 @@ namespace BackendLibrary
         }
 
         // Combine shared logic between constructors
-        private void InitializeFromPdf(PdfDocument pdf, PdfTextExtractor pdfTextExtractor, PdfFileNameExtractor pdfFileNameExtractor, string? pdfPath)
+        private void InitializeFromPdf(PdfDocument pdf, PdfTextExtractor pdfTextExtractor, PdfFileNameExtractor pdfFileNameExtractor)
         {
             // OwnerPassword property needs a password to set SecuritySettings
             pdf.SecuritySettings.OwnerPassword = "admin";
@@ -114,6 +112,7 @@ namespace BackendLibrary
 
             try
             {
+                PageNames = pdfTextExtractor.ExtractPageNames();
                 FileNamePieceMark = pdfFileNameExtractor.GetFileNamePieceMark();
                 ProjectNumber = pdfTextExtractor.ExtractProjectNumber();
                 ProjectName = pdfTextExtractor.ExtractProjectName();
@@ -122,14 +121,6 @@ namespace BackendLibrary
                 PiecesRequired = pdfTextExtractor.ExtractPiecesRequired();
                 Weight = pdfTextExtractor.ExtractWeight();
                 DesignNumber = pdfTextExtractor.ExtractDesignNumber();
-
-                ////Extract Page Names using PdfPageNameExtractor
-                //if (!string.IsNullOrEmpty(pdfPath) && File.Exists(pdfPath))
-                //{
-                //    var pageNameExtractor = new PdfPageNameExtractor(72); // top 1 inch band
-                //    TimeSpan elapsed;
-                //    PageNames = pageNameExtractor.ExtractAll(pdfPath, out elapsed).ToList();
-                //}
             }
             catch (Exception ex)
             {
@@ -140,7 +131,9 @@ namespace BackendLibrary
         public override string ToString()
         {
             String str =
+                dateTimeExtracted + "\n" +
                 "NumberOfPages: " + NumberOfPages + "\n" +
+                "PageNames: " + string.Join(", ", PageNames) + "\n" +
                 "FileName: " + FileName + "\n" +
                 "FileNamePieceMark: " + FileNamePieceMark + "\n" +
                 "ProjectNumber: " + ProjectNumber + "\n" +
@@ -155,7 +148,7 @@ namespace BackendLibrary
                 "FormViewRectangleY: " + FormViewRectangleY + "\n" +
                 "FormViewRectangleWidth: " + FormViewRectangleWidth + "\n" +
                 "FormViewRectangleHeight: " + FormViewRectangleHeight + "\n";
-            //"PageNames: " + (PageNames.Count > 0 ? string.Join(" | ", PageNames) : "null") + "\n";
+
 
             return str;
         }
