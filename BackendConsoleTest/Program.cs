189fef5953980890230d4c3690740da8bf37c63d
coreyellow <lancemiguel@gmail.com>
2025-11-05 09:37:58 -0500

    Add dateTimeExtracted to ShopTicket

--- BackendConsoleTest/Program.cs
+++ BackendConsoleTest/Program.cs
@@ -1,8 +1,5 @@
 ﻿using BackendLibrary;
-using PdfSharp.Pdf;
-using PdfSharp.Pdf.IO;
-using System.IO;
-using SQL3cs;
+//using SQL3cs;
 
 class Program
 {
@@ -12,13 +9,13 @@ class Program
         //String filePath = "/Users/zacharycritchfield/Desktop/ShopTickets/ShopTicketData/pdfs/20-NE0881-W001_P2.pdf";
         //String filePath = "/Users/zacharycritchfield/Desktop/ShopTickets/ShopTicketData/pdfs/24-NE1096-DT023_P0.pdf";
         //string filePath = "/Users/zacharycritchfield/Desktop/ShopTickets/ShopTicketData/pdfs/25-NE1203.01-W015_P2.pdf";
-        String filePath = "C:/Users/lance/Documents/ShopTicketData/pdfs/24-NE1173-W038_P2.pdf";
-        //String filePath = "";
+        //String filePath = "C:/Users/lance/Documents/ShopTicketData/pdfs/24-NE1173-W038_P2.pdf";
+        String filePath = "";
 
         byte[] pdfBytes = File.ReadAllBytes(filePath);
         String pdfName = Path.GetFileNameWithoutExtension(filePath);
         ShopTicket pdf = new ShopTicket(pdfName, pdfBytes, "../../../../BackendLibrary/best.onnx");
-        CustomerData db = new CustomerData();
+        //CustomerData db = new CustomerData();
 
         //ShopTicket pdf = new ShopTicket(filePath);
 
--- BackendLibrary/ShopTicket.cs
+++ BackendLibrary/ShopTicket.cs
@@ -5,22 +5,23 @@ namespace BackendLibrary
 {
     public class ShopTicket
     {
-        public byte[] PdfBytes;
-        public int NumberOfPages { get; set; }                     // Number of pages in the PDF file.
-        public string[] PageNames { get; set; }                    // Page names extracted from view labels.
-        public string FileName { get; set; }                       // File name of the PDF file.
-        public string? FileNamePieceMark { get; set; }             // Piece Mark extracted from the file name.
-        public string ProjectNumber { get; set; }                  // Project Number from the title block labelled "JOB NO.".
-        public string ProjectName { get; set; }                    // Project Name from the title block labelled "PROJECT:".
-        public string FileContentPieceMark { get; set; }           // Piece Mark from the title block labelled "PIECE MARK".
-        public string[]? ControlNumbers { get; set; }              // Control numbers from the square above the title block labelled "CONTROL NO.:".
-        public int PiecesRequired { get; set; }                    // Pieces required from the title block labelled "PIECES REQ'D:".
-        public decimal Weight { get; set; }                        // Weight from the title block labelled "WEIGHT:".
-        public string DesignNumber { get; set; }                   // Design number from the title block labelled "DESIGN:".
-        public int RectanglePage { get; set; }                     // 0-based index of the page containing form and section view rectangles.
-        public double FormViewRectangleX { get; set; }             // Distance from left edge of PDF to left edge of the form view rectangle (inches).
-        public double FormViewRectangleY { get; set; }             // Distance from top edge of PDF to top edge of the form view rectangle (inches).
-        public double FormViewRectangleWidth { get; set; }         // Width of the form view rectangle (inches).
+        public byte[] PdfBytes { get; }                             // Bytearray of PDF file
+        public DateTime dateTimeExtracted { get; }                  // Date and time when ShopTicket was constructed
+        public int NumberOfPages { get; set; }                      // Number of pages in the PDF file.
+        public string[] PageNames { get; set; }                     // Page names extracted from view labels.
+        public string FileName { get; set; }                        // File name of the PDF file.
+        public string? FileNamePieceMark { get; set; }              // Piece Mark extracted from the file name.
+        public string ProjectNumber { get; set; }                   // Project Number from the title block labelled "JOB NO.".
+        public string ProjectName { get; set; }                     // Project Name from the title block labelled "PROJECT:".
+        public string FileContentPieceMark { get; set; }            // Piece Mark from the title block labelled "PIECE MARK".
+        public string[]? ControlNumbers { get; set; }               // Control numbers from the square above the title block labelled "CONTROL NO.:".
+        public int PiecesRequired { get; set; }                     // Pieces required from the title block labelled "PIECES REQ'D:".
+        public decimal Weight { get; set; }                         // Weight from the title block labelled "WEIGHT:".
+        public string DesignNumber { get; set; }                    // Design number from the title block labelled "DESIGN:".
+        public int RectanglePage { get; set; }                      // 0-based index of the page containing form and section view rectangles.
+        public double FormViewRectangleX { get; set; }              // Distance from left edge of PDF to left edge of the form view rectangle (inches).
+        public double FormViewRectangleY { get; set; }              // Distance from top edge of PDF to top edge of the form view rectangle (inches).
+        public double FormViewRectangleWidth { get; set; }          // Width of the form view rectangle (inches).
         public double FormViewRectangleHeight { get; set; }        // Height of the form view rectangle (inches).
         //public double SectionViewRectangleX { get; set; }          // Distance from left edge of PDF to left edge of the section view rectangle (inches).
         //public double SectionViewRectangleY { get; set; }          // Distance from top edge of PDF to top edge of the section view rectangle (inches).
@@ -84,7 +85,7 @@ namespace BackendLibrary
                 InitializeFromPdf(pdf, pdfTextExtractor, PdfFileNameExtractor.InitializeWithFileName(fileName));
 
                 (RectanglePage, FormViewRectangleX, FormViewRectangleY, FormViewRectangleWidth, FormViewRectangleHeight) = Detect.GetRectInfo(modelPath, fileName, stream);
-
+                dateTimeExtracted = DateTime.Now;
             }
             catch (Exception ex)
             {
@@ -130,6 +131,7 @@ namespace BackendLibrary
         public override string ToString()
         {
             String str =
+                dateTimeExtracted + "\n" +
                 "NumberOfPages: " + NumberOfPages + "\n" +
                 "PageNames: " + string.Join(", ", PageNames) + "\n" +
                 "FileName: " + FileName + "\n" +
