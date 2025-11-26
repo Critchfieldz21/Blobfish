3a43277e42636ae83e59c6764150c9c0fe42ada3
coreyellow <lancemiguel@gmail.com>
2025-11-26 18:07:14 -0500

    Preload PdfSharp

--- TestBlazor/BlazorApp1/Services/PreloadService.cs
+++ TestBlazor/BlazorApp1/Services/PreloadService.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using PdfSharp.Pdf.Annotations;
 
 namespace BackendLibrary
@@ -6,59 +7,55 @@ namespace BackendLibrary
     public class PreloadService
     {
         // Dummy PDF Content
-        private static byte[] _tinyPdf => Encoding.ASCII.GetBytes(
-                @"%PDF-1.4
-                1 0 obj << /Type /Catalog /Pages 2 0 R >> endobj
-                2 0 obj << /Type /Pages /Kids [3 0 R] /Count 1 >> endobj
-                3 0 obj << /Type /Page /Parent 2 0 R /MediaBox [0 0 200 200] >> endobj
-                xref
-                0 4
-                0000000000 65535 f 
-                0000000010 00000 n 
-                0000000060 00000 n 
-                0000000110 00000 n 
-                trailer << /Root 1 0 R /Size 4 >>
-                startxref
-                160
-                %%EOF"
-            );
+        private static byte[] _tinyPdf => File.ReadAllBytes("Services/tinypdf.pdf");
 
         public static void PreloadPackages(ILogger<PreloadService> logger)
         {
-            PreloadPdfPig(logger);
-            PreloadPdfiumViewer(logger);
+            var jobs = new (Action Preload, string label)[]
+            {
+                (() =>  PreloadPdfPig(),            "PdfPig"),
+                (() =>  PreloadPdfiumViewer(),      "PdfiumViewer"),
+                (() =>  PreloadPdfSharp(),         "PdfSharp"),
+            };
+
+            Parallel.Invoke(
+                jobs.Select(job => (Action)(() =>
+                {
+                    try
+                    {
+                        job.Preload();
+                        logger.LogDebug("Preloaded {job.label} sucessfully.", job.label);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogDebug("Failed to load {job.label}: {ex.Message}.", job.label, ex.Message);
+                    }
+                }))
+                .ToArray()
+            );
         }
 
-        private static void PreloadPdfPig(ILogger<PreloadService> logger)
+        private static void PreloadPdfPig()
         {     
-            try
-            {
-                using var pdf = UglyToad.PdfPig.PdfDocument.Open(_tinyPdf);
-                _ = pdf.NumberOfPages;
-                List<UglyToad.PdfPig.Content.Page> pages = pdf.GetPages().ToList();
-                _ = pages[0].GetWords();
-                logger.LogInformation("Preloaded PdfPig successfully.");
-            }
-            catch
-            {
-                logger.LogError("Failed to load PdfPig.");
-            }
+            using var pdf = UglyToad.PdfPig.PdfDocument.Open(_tinyPdf);
+            _ = pdf.NumberOfPages;
+            List<UglyToad.PdfPig.Content.Page> pages = pdf.GetPages().ToList();
+            _ = pages[0].GetWords();
         }
 
-        private static void PreloadPdfiumViewer(ILogger<PreloadService> logger)
+        private static void PreloadPdfiumViewer()
         {
-            try
-            {
-                MemoryStream stream = new MemoryStream(_tinyPdf);
-                using var document = PdfiumViewer.PdfDocument.Load(stream);
-                int pageCount = document.PageCount;
-                using var image = document.Render(0, 72, 72, true);
-                logger.LogInformation("Preloaded PdfiumViewer successfully.");
-            }
-            catch
-            {
-                logger.LogError("Failed to load PdfiumViewer.");
-            }
+            MemoryStream stream = new MemoryStream(_tinyPdf);
+            using var document = PdfiumViewer.PdfDocument.Load(stream);
+            _ = document.PageCount;
+            _ = document.Render(0, 72, 72, true);
+        }
+
+        private static void PreloadPdfSharp()
+        {
+            using var stream = new MemoryStream(_tinyPdf);
+            using var document = PdfSharp.Pdf.IO.PdfReader.Open(stream, PdfSharp.Pdf.IO.PdfDocumentOpenMode.Import);
+            _ = document.PageCount;
         }
     }
 }
