9f5e19eb4d42e5bcc353a5fa51760d79b79d8435
coreyellow <lancemiguel@gmail.com>
2025-12-05 17:12:05 -0500

    Change setters in ShopTicket fields

--- TestBlazor/BlazorApp1/Services/ShopTicket.cs
+++ TestBlazor/BlazorApp1/Services/ShopTicket.cs
@@ -14,25 +14,24 @@ namespace BackendLibrary
         private ILogger<ShopTicket> _logger;
 
         public byte[] PdfBytes { get; }                             // Bytearray of PDF file
-
-        public byte[] annotatedPdfBytes { get; set; }                 // Bytearray of annotated PDF file with rectangles drawn
+        public byte[] annotatedPdfBytes { get; }                    // Bytearray of annotated PDF file with rectangles drawn
         public DateTime dateTimeExtracted { get; }                  // Date and time when ShopTicket was constructed
-        public int NumberOfPages { get; set; }                      // Number of pages in the PDF file.
-        public string[] PageNames { get; set; }                     // Page names extracted from view labels.
-        public string FileName { get; set; }                        // File name of the PDF file.
-        public string? FileNamePieceMark { get; set; }              // Piece Mark extracted from the file name.
-        public string ProjectNumber { get; set; }                   // Project Number from the title block labelled "JOB NO.".
-        public string ProjectName { get; set; }                     // Project Name from the title block labelled "PROJECT:".
-        public string FileContentPieceMark { get; set; }            // Piece Mark from the title block labelled "PIECE MARK".
-        public string[]? ControlNumbers { get; set; }               // Control numbers from the square above the title block labelled "CONTROL NO.:".
-        public int PiecesRequired { get; set; }                     // Pieces required from the title block labelled "PIECES REQ'D:".
-        public decimal Weight { get; set; }                         // Weight from the title block labelled "WEIGHT:".
-        public string DesignNumber { get; set; }                    // Design number from the title block labelled "DESIGN:".
-        public int RectanglePage { get; set; }                      // 0-based index of the page containing form and section view rectangles.
-        public double FormViewRectangleX { get; set; }              // Distance from left edge of PDF to left edge of the form view rectangle (inches).
-        public double FormViewRectangleY { get; set; }              // Distance from top edge of PDF to top edge of the form view rectangle (inches).
-        public double FormViewRectangleWidth { get; set; }          // Width of the form view rectangle (inches).
-        public double FormViewRectangleHeight { get; set; }         // Height of the form view rectangle (inches).
+        public int NumberOfPages { get; private set; }              // Number of pages in the PDF file.
+        public string[] PageNames { get; private set; }             // Page names extracted from view labels.
+        public string FileName { get; private set; }                // File name of the PDF file.
+        public string? FileNamePieceMark { get; private set; }      // Piece Mark extracted from the file name.
+        public string ProjectNumber { get; private  set; }          // Project Number from the title block labelled "JOB NO.".
+        public string ProjectName { get; private set; }             // Project Name from the title block labelled "PROJECT:".
+        public string FileContentPieceMark { get; private set; }    // Piece Mark from the title block labelled "PIECE MARK".
+        public string[]? ControlNumbers { get; private set; }       // Control numbers from the square above the title block labelled "CONTROL NO.:".
+        public int PiecesRequired { get; private set; }             // Pieces required from the title block labelled "PIECES REQ'D:".
+        public decimal Weight { get; private set; }                 // Weight from the title block labelled "WEIGHT:".
+        public string DesignNumber { get; private set; }            // Design number from the title block labelled "DESIGN:".
+        public int RectanglePage { get; private set; }              // 0-based index of the page containing form and section view rectangles.
+        public double FormViewRectangleX { get; private set; }      // Distance from left edge of PDF to left edge of the form view rectangle (inches).
+        public double FormViewRectangleY { get; private set; }      // Distance from top edge of PDF to top edge of the form view rectangle (inches).
+        public double FormViewRectangleWidth { get; private set; }  // Width of the form view rectangle (inches).
+        public double FormViewRectangleHeight { get; private set; }  // Height of the form view rectangle (inches).
         //public double SectionViewRectangleX { get; set; }          // Distance from left edge of PDF to left edge of the section view rectangle (inches).
         //public double SectionViewRectangleY { get; set; }          // Distance from top edge of PDF to top edge of the section view rectangle (inches).
         //public double SectionViewRectangleWidth { get; set; }      // Width of the section view rectangle (inches).
@@ -64,17 +63,17 @@ namespace BackendLibrary
         /// <summary>
         /// ShopTicket constructor initializing from PDF byte array.
         /// </summary>>
-        public ShopTicket(ILoggerFactory loggerFactory, String fileName, byte[] pdfBytes)
+        public ShopTicket(ILoggerFactory loggerFactory, String fileName, byte[] byteArray)
         {
             try
             {
                 _loggerFactory = loggerFactory;
                 _logger = _loggerFactory.CreateLogger<ShopTicket>();
-                PdfBytes = pdfBytes;
+                PdfBytes = byteArray;
                 FileName = fileName;
 
                 // Use PdfSharp PdfReader to initialize a PdfDocument object off of pdf byte array
-                MemoryStream stream = new MemoryStream(pdfBytes);
+                MemoryStream stream = new MemoryStream(byteArray);
                 PdfDocument pdf = PdfReader.Open(stream, PdfDocumentOpenMode.Import);
 
                 InitializeFromPdf(pdf);
@@ -88,7 +87,7 @@ namespace BackendLibrary
         /// <summary>
         /// ShopTicket constructor initializing from PDF byte array with form view rectangle detection.
         /// </summary>
-        public ShopTicket(ILoggerFactory loggerFactory, String fileName, byte[] pdfBytes, string modelPath)
+        public ShopTicket(ILoggerFactory loggerFactory, String fileName, byte[] byteArray, string modelPath)
         {
             try
             {
@@ -96,11 +95,11 @@ namespace BackendLibrary
                 _logger = _loggerFactory.CreateLogger<ShopTicket>();
                 _logger.LogDebug("Starting ShopTicket construction for {FileName}", fileName);
                
-                PdfBytes = pdfBytes;
+                PdfBytes = byteArray;
                 FileName = fileName;
 
                 // Use PdfSharp PdfReader to initialize a PdfDocument object off of pdf byte array
-                MemoryStream stream = new MemoryStream(pdfBytes);
+                MemoryStream stream = new MemoryStream(byteArray);
                 _logger.LogDebug("MemoryStream created from PdfBytes");
 
                 PdfDocument pdf = PdfReader.Open(stream, PdfDocumentOpenMode.Import);
